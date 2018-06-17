e724bcae96735436c1b1b2249486a0b6d69420ef
robsouthgate4 <robsouthgate4@gmail.com>
2018-06-17 21:15:04 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerMovementSystem.cs
@@ -0,0 +1,14 @@
+﻿
+using Unity.Entities;
+using UnityEngine;
+
+public class PlayerMovementSystem : ComponentSystem {
+
+    private struct Filter {
+        public Rigidbody Rigidbody;
+    }
+
+    protected override void OnUpdate() {
+        throw new System.NotImplementedException();
+    }
+}
